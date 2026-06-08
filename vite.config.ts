24ff9d304d191bb9f84cc002b063e07d05c49f87
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2026-06-08 13:11:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    nitro: true,
+    nitro: {
+      preset: "vercel"
+    },
     server: { entry: "server" },
   },
 });
